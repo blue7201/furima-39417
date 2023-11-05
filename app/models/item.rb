9f4aecb75a8ac90efb2a7fee7dc0edcdf915257b
blue7201 <ogawa2233@gmail.com>
2023-11-06 05:45:13 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,10 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :state
+  belongs_to :postage
+  belongs_to :region
+  belongs_to :shipping_date
 
   belongs_to :user
   has_one_attached :image
@@ -6,11 +12,11 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :item_name
     validates :description
-    validates :category_id
-    validates :state_id
-    validates :postage_id
-    validates :region_id
-    validates :shipping_date_id
+    validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+    validates :state_id, numericality: { other_than: 1 , message: "can't be blank"}
+    validates :postage_id, numericality: { other_than: 1 , message: "can't be blank"}
+    validates :region_id, numericality: { other_than: 1 , message: "can't be blank"}
+    validates :shipping_date_id, numericality: { other_than: 1 , message: "can't be blank"}
     validates :price
     validates :image
 

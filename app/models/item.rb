b305b5502d5a9e44b4292279d6a55ee282e68749
blue7201 <ogawa2233@gmail.com>
2023-11-06 14:51:42 +0900

    商品出品ページの表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -19,5 +19,5 @@ class Item < ApplicationRecord
     validates :shipping_date_id, numericality: { other_than: 1 , message: "can't be blank"}
     validates :price
     validates :image
-
+  end
 end

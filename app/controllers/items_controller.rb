3095c62a91430b8b4e1ceee3a3790ee75564d5a0
blue7201 <ogawa2233@gmail.com>
2023-11-06 23:24:26 +0900

    Rubocopによる修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,7 +21,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:image, :item_name, :description, :category_id, :state_id, :postage_id, :region_id, :shipping_date_id, :price ).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :item_name, :description, :category_id, :state_id, :postage_id, :region_id,
+                                 :shipping_date_id, :price).merge(user_id: current_user.id)
   end
-
 end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -10,7 +10,7 @@ FactoryBot.define do
     region_id { Faker::Number.between(from: 2, to: 48) }
     shipping_date_id { Faker::Number.between(from: 2, to: 4) }
     price { Faker::Number.between(from: 300, to: 9_999_999) }
-  
+
     after(:build) do |item|
       item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
     end

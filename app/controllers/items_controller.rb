5c4a0ffe4ac72c1ab4bcf3470538ce72ba053979
blue7201 <ogawa2233@gmail.com>
2023-11-06 16:39:57 +0900

    出品商品情報の保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,7 +20,7 @@ class ItemsController < ApplicationController
 
   private
 
-  def message_params
+  def item_params
     params.require(:item).permit(:image, :item_name, :description, :category_id, :state_id, :postage_id, :region_id, :shipping_date_id, :price ).merge(user_id: current_user.id)
   end
 

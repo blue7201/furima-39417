99946c8e40e93a2ade19aedaf3a67d880d6d42d9
blue7201 <ogawa2233@gmail.com>
2023-11-10 02:55:58 +0900

    before_actionの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :search_item, only: [:show, :edit, :update]
+  before_action :move_to_index, only: [:edit, :update]
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -40,5 +42,13 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :item_name, :description, :category_id, :state_id, :postage_id, :region_id,
                                  :shipping_date_id, :price).merge(user_id: current_user.id)
+  end
+
+  def search_item
+    @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    redirect_to action: :index unless current_user.id == @item.user_id
   end
 end

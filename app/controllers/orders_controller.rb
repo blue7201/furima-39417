1cea2d56805cd550fbf3cd02d5883d91418ba44f
blue7201 <ogawa2233@gmail.com>
2023-11-13 17:52:38 +0900

    コントローラーの作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,14 +1,20 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :move_to_index, only: [:index, :create]
+
   def index
+    #gon.public_key = ENV['PAYJP_PUBLIC_KEY']
     @order_shipment = OrderShipment.new
   end
 
   def create
     @order_shipment = OrderShipment.new(order_params)
     if @order_shipment.valid?
+      #pay_item
       @order_shipment.save
       redirect_to root_path
     else
+      #gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
       render :index, status: :unprocessable_entity
     end
   end
@@ -17,5 +23,19 @@ class OrdersController < ApplicationController
 
   def order_params
     params.require(:order_shipment).permit(:postalcode, :region_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id)
+  end
+
+  #def pay_item
+    #Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    #Payjp::Charge.create(
+      #amount: @item.price,
+      #card: order_params[:token],
+      #currency: 'jpy'
+    #)
+  #end
+
+  def move_to_index
+    @item = Item.find(params[:item_id])
+    #redirect_to root_path if current_user.id == @item.user_id || @item.order.present?
   end
 end

3fda907a240363ecabdaf87396a3ccb4fdf5334a
blue7201 <ogawa2233@gmail.com>
2023-11-13 17:53:16 +0900

    データベースの作成

--- db/migrate/20231112065702_create_shipments.rb
+++ db/migrate/20231112065702_create_shipments.rb
@@ -1,7 +1,13 @@
 class CreateShipments < ActiveRecord::Migration[7.0]
   def change
     create_table :shipments do |t|
-
+      t.references :order, null: false, foreign_key: true
+      t.string :postalcode, null: false
+      t.integer :region_id, null: false
+      t.string :city, null: false
+      t.string :block, null: false
+      t.string :building
+      t.string :phone_number, null: false
       t.timestamps
     end
   end
